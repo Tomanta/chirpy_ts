0727093f961773eedf294b92bae00d7b04b1b3a7
Tomanta <bzednick@gmail.com>
2025-06-29 12:03:51 -0500

    Added logging middleware

---
+++ src/api/middleware.ts
@@ -0,0 +1,11 @@
+import type { Request, Response, NextFunction } from "express";
+
+export function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        
+        if (res.statusCode >= 400) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        } 
+    });
+    next();
+}
