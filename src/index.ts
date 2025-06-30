5f40a61957103a94d6bd12e5e98a23bb88d53cfb
Tomanta <bzednick@gmail.com>
2025-06-30 17:45:38 -0500

    Added chirp validation endpoint

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { handlerMetrics, handlerReset } from "./api/metrics.js";
 import { middlewareLogResponses, middlewareMetricsInc } from "./api/middleware.js";
+import { handleValidateChirp } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
@@ -11,6 +12,7 @@ app.get("/admin/metrics", handlerMetrics);
 app.post("/admin/reset", handlerReset);
 
 app.get("/api/healthz", handlerReadiness);
+app.post("/api/validate_chirp", handleValidateChirp);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.listen(PORT, () => {
