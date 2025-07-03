c6cb64e70234da130bba48894dbd270fff38ca62
Tomanta <bzednick@gmail.com>
2025-07-03 15:49:10 -0500

    Added user table, refactored

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "./src/config.js";
 
 export default defineConfig({
     schema: "src/lib/db/schema.ts",
     out: "src/lib/db/migrations",
     dialect: "postgresql",
     dbCredentials: {
-        url: "postgres://postgres:postgres@localhost:5432/chirpy?sslmode=disable",
+        url: config.dbURL,
     },
 });
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
+import { loadEnvFile, env } from "process";
+
+loadEnvFile();
+
+function envOrThrow(key: string) {
+    if (!env[key]) {
+        throw new Error(`Config key does not exist: ${key}`);
+    }
+    return env[key];
+}
+
 type APIConfig = {
     fileServerHits: number;
+    dbURL: string;
 };
 
 export const config: APIConfig = {
     fileServerHits: 0,
+    dbURL: envOrThrow("dbUrl"),
 }
